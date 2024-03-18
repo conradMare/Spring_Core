b3962a2b669f9173ee2b7b4b0f01c8067d78aff9
conradMare <conradmare4@gmail.com>
2024-03-18 07:10:24 +0200

    2024/03/18(am)
    Section07 - Updated

--- RESTful Web Services with Spring Framework (Udemy)/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserDetailsRequestModel.java
+++ RESTful Web Services with Spring Framework (Udemy)/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserDetailsRequestModel.java
@@ -3,13 +3,22 @@ package com.appsdeveloperblog.app.ws.ui.model.request;
 // import javax.validation.constraints.NotNull;
 
 public class UserDetailsRequestModel {
-	// @NotNull
+	
+	// @NotNull(message="First name can not be null")
+	// @Size(min=2, message="First name must not be less than 2 characters")
 	private String firstName;
-	// @NotNull
+	
+	// @NotNull(message="Last name can not be null")
+	// @Size(min=2, message="Last name must not be less than 2 characters")
 	private String lastName;
-	// @NotNull
+	
+	// @NotNull(message="Email can not be null")
+	// @Email
 	private String email;
-	// @Size
+	
+	// @NotNull(message="Password can not be null")
+	// @Size(min=8,max=16, message="Password must be equal or greater than 8 characters and " +
+	//							   "less than 16 characters")
 	private String password;
 
 	public String getFirstName() {
