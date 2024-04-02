65b6623e3e06dcb4020259db31a15cdf0d18a6d3
conradMare <conradmare4@gmail.com>
2024-04-02 13:26:38 +0200

    2024/04/02(pm)
    RunnerzApplication - Project Code Setup

--- Building web applications in Java with Spring Boot 3/runnerz/src/main/java/dev/danvega/runnerz/RunnerzApplication.java
+++ Building web applications in Java with Spring Boot 3/runnerz/src/main/java/dev/danvega/runnerz/RunnerzApplication.java
@@ -1,13 +1,40 @@
 package dev.danvega.runnerz;
 
+import dev.danvega.runnerz.run.Location;
+import dev.danvega.runnerz.run.Run;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class RunnerzApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RunnerzApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(RunnerzApplication.class, args);
+
+
+    }
+
+    // Command Line Runner - Starts after the application has started (Application Context of Beans)
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            Run run = new Run(
+                    1,
+                    "First Run",
+                    LocalDateTime.now(),
+                    LocalDateTime.now().plus(1, ChronoUnit.HOURS),
+                    5,
+                    Location.OUTDOOR);
+            log.info("Run" + run);
+        };
+    }
 }
