433bdfae166098714cb360f823f465771491b90c
conradMare <conradmare4@gmail.com>
2024-04-03 13:28:13 +0200

    2024/04/03(pm)
    RunnerzApplication - Create RunRepository, Update RunController to display data in the browser

--- Building web applications in Java with Spring Boot 3/runnerz/src/main/java/dev/danvega/runnerz/run/RunController.java
+++ Building web applications in Java with Spring Boot 3/runnerz/src/main/java/dev/danvega/runnerz/run/RunController.java
@@ -3,12 +3,27 @@ package dev.danvega.runnerz.run;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class RunController {
 
-    @GetMapping("/hello")
-    String home() {
+    // Get an instance that returns the RunRepository:
 
-        return "Hello World!";
+//    WRONG WAY TO GET AN INSTANCE -> Spring is an inversion of control framework(note the 'new' keyword):
+//    private final RunRepository runRepository {
+//        this.runRepository = new RunRepository();
+//    }
+
+    //    DO THIS:
+    private final RunRepository runRepository;
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    // Write a method that returns a List of runs:
+    @GetMapping("api/runs")
+    List<Run> findAll() {
+        return runRepository.findAll();
     }
 }
