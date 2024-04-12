bc44e2a461c17d1fc11ed78ab2a7fddc896714cc
conradMare <conradmare4@gmail.com>
2024-04-12 10:41:05 +0200

    2024/04/12(am)
    RunnerzFitnessApplication - Module4_Working with Databases

--- Building web applications in Java with Spring Boot 3/RunnerzFitnessApplication/src/main/java/dev/danvega/RunnerzFitnessApplication/Application.java
+++ Building web applications in Java with Spring Boot 3/RunnerzFitnessApplication/src/main/java/dev/danvega/RunnerzFitnessApplication/Application.java
@@ -1,16 +1,9 @@
 package dev.danvega.RunnerzFitnessApplication;
 
-import dev.danvega.RunnerzFitnessApplication.run.Location;
-import dev.danvega.RunnerzFitnessApplication.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class Application {
@@ -22,17 +15,17 @@ public class Application {
 
     }
 
-    @Bean
-    CommandLineRunner runner() {
-        return args -> {
-            Run run = new Run(
-                    1,
-                    "First Run",
-                    LocalDateTime.now(),
-                    LocalDateTime.now().plus(1, ChronoUnit.HOURS),
-                    5,
-                    Location.OUTDOOR);
-            log.info("Run: " + run);
-        };
-    }
+//    @Bean
+//    CommandLineRunner runner(RunRepository runRepository) {
+//        return args -> {
+//            Run run = new Run(
+//                    1,
+//                    "First Run",
+//                    LocalDateTime.now(),
+//                    LocalDateTime.now().plus(1, ChronoUnit.HOURS),
+//                    5,
+//                    Location.OUTDOOR);
+//            runRepository.create(run);
+//        };
+//    }
 }
---
+++ Building web applications in Java with Spring Boot 3/RunnerzFitnessApplication/src/main/java/dev/danvega/RunnerzFitnessApplication/run/Runs.java
@@ -0,0 +1,6 @@
+package dev.danvega.RunnerzFitnessApplication.run;
+
+import java.util.List;
+
+public record Runs(List<Run> runs) {
+}
