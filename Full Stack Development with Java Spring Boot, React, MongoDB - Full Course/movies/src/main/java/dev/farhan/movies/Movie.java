022059561affd7dcf5094b3ff5d55ad95389c51d
conradMare <conradmare4@gmail.com>
2024-04-19 09:36:14 +0200

    2024/04/19(am)
    Movies - Movies and Reviews End-Points

---
+++ Full Stack Development with Java Spring Boot, React, MongoDB - Full Course/movies/src/main/java/dev/farhan/movies/Movie.java
@@ -0,0 +1,27 @@
+package dev.farhan.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+public class Movie {
+    @Id // Unique identifier for each movie in the database
+    private ObjectId id;
+
+    private String imdbId;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailerLink;
+
+    private String poster;
+
+    private List<String> genres;
+
+    private String backdrops;
+}
