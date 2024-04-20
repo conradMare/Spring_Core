a2f31f879dc439ccccdd36a173b2ebf21fb9525a
conradMare <conradmare4@gmail.com>
2024-04-20 06:08:13 +0200

    2024/04/20(am)
    Movies - Movie model, Review model, Movie controller.

--- Full Stack Development with Java Spring Boot, React, MongoDB - Full Course/movies/src/main/java/dev/farhan/movies/Movie.java
+++ Full Stack Development with Java Spring Boot, React, MongoDB - Full Course/movies/src/main/java/dev/farhan/movies/Movie.java
@@ -1,12 +1,19 @@
 package dev.farhan.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
 @Document(collection = "movies")
+@Data // Instead of writing Getters, Setters and toString methods
+@AllArgsConstructor // Takes all the private fields as arguments and creates a constructor
+@NoArgsConstructor // Constructor that takes no parameters
 public class Movie {
     @Id // Unique identifier for each movie in the database
     private ObjectId id;
@@ -24,4 +31,7 @@ public class Movie {
     private List<String> genres;
 
     private String backdrops;
+
+    @DocumentReference // Will cause to store only the id's of the reviews, and the reviews will be in a separate collection
+    private List<Review> reviewsIds;
 }
