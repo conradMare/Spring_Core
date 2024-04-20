6dd30542b0007e765af99503a65d66e324fa2ef9
conradMare <conradmare4@gmail.com>
2024-04-20 06:46:26 +0200

    2024/04/20(am)
    Movies - Service class and Repository interface.

---
+++ Full Stack Development with Java Spring Boot, React, MongoDB - Full Course/movies/src/main/java/dev/farhan/movies/MovieService.java
@@ -0,0 +1,16 @@
+package dev.farhan.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    @Autowired // To instantiate the class for us
+    private MovieRepository movieRepository;
+
+    public  List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+}
