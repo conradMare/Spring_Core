830ba051f586b9c8706318ec00fbc0dce7c37fac
conradMare <conradmare4@gmail.com>
2024-04-24 06:12:33 +0200

    2024/04/24(am)
    backend - CORS configuration

---
+++ How to easily secure Your Fullstack ReactJs - Sping Boot Application with JWT Authentication/backend/src/main/java/com/sergio/jwt/backend/config/WebConfig.java
@@ -0,0 +1,48 @@
+package com.sergio.jwt.backend.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // Specify everything coming from the frontend:
+        config.setAllowCredentials(true);
+        // Credentials sent by the frontend (accept them):
+        config.addAllowedOrigin("http://localhost:3000");
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT
+        ));
+        config.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()
+        ));
+        // Time options request is accepted (30 minutes)
+        config.setMaxAge(3600L);
+        // Applied for all the requests:
+        source.registerCorsConfiguration("/**", config);
+        // Bean at the lowest position to be used before any Spring security filter:
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(-102);
+        return bean;
+    }
+}
