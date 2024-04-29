4a8b13c4166ba62cf2aa5a556057164472d43580
conradMare <conradmare4@gmail.com>
2024-04-29 07:49:51 +0200

    2024/04/29(am)
    New Spring Project - "SpringBoot+React_JWTAuthentication"

---
+++ SpringBoot+React_JWTAuthentication/Spring-Boot-React-JWT/src/main/java/com/example/Spring/Boot/React/JWT/models/ERole.java
@@ -0,0 +1,7 @@
+package com.example.Spring.Boot.React.JWT.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ SpringBoot+React_JWTAuthentication/Spring-Boot-React-JWT/src/main/java/com/example/Spring/Boot/React/JWT/models/Role.java
@@ -0,0 +1,25 @@
+package com.example.Spring.Boot.React.JWT.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    // getters and setters
+}
